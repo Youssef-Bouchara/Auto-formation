5d72e330395fd6e75914ff431be509bb1debb130
user <user@HP-YOUSSEF.travail.gov.ma>
2018-11-28 12:40:54 +0100

    Final project (Rest 2 - Angular 4)

---
+++ demo/src/main/java/org/sid/DemoApplication.java
@@ -0,0 +1,23 @@
+package org.sid;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.sid.dao.ContactRepository;
+import org.sid.entities.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	
+	
+}
